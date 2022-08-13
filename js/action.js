690e5f8992a017ab6c9cd022b69b12d1e6da945b
rater31 <fedehidalgo89@gmail.com>
2022-08-13 19:30:25 -0300

    1

---
+++ js/action.js
@@ -0,0 +1,7 @@
+const numeros = document.querySelector(".numeros");
+const operaciones = document.querySelector(".operaciones");
+const pantalla =document.querySelector(".pantalla");
+
+numeros.addEventListener('click',()=>{
+    
+})
